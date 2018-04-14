554d003b572d09d639b48bc87b46f6d0eece2a2a
ndodson <ndodson@syr.edu>
2018-04-13 20:01:00 -0400

    cosine similarity update

---
+++ cosSimilarity3.py
@@ -0,0 +1,52 @@
+import re, math
+from collections import Counter
+import math
+
+WORD = re.compile(r'\w+')
+
+
+#cosine of 2 vectors function
+def get_cosine(vec1, vec2):
+     intersection = set(vec1.keys()) & set(vec2.keys())
+     numerator = sum([vec1[x] * vec2[x] for x in intersection])
+
+     sum1 = sum([vec1[x]**2 for x in vec1.keys()])
+     sum2 = sum([vec2[x]**2 for x in vec2.keys()])
+     denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+     if not denominator:
+        return 0.0
+     else:
+        return float(numerator) / denominator
+
+#text to vector representation
+def text_to_vector(text):
+     words = WORD.findall(text)
+     return Counter(words)
+
+#declare some sentences
+text1 = 'This is a foo bar sentence .'
+text2 = 'This sentence is similar to a foo bar sentence .'
+text3 = 'have fun figuring this out! mwahaha'
+
+#call text to vector
+vector1 = text_to_vector(text1)
+vector2 = text_to_vector(text2)
+vector3 = text_to_vector(text3)
+
+
+#calculate cosine with vector form inputs
+cosine1 = get_cosine(vector1, vector2)
+cosine2 = get_cosine(vector1, vector3)
+
+
+#format the output
+angle_in_radians = math.acos(cosine1)
+degrees1 = math.degrees(angle_in_radians)
+
+angle_in_radians = math.acos(cosine2)
+degrees2 =  math.degrees(angle_in_radians)
+
+#return a similarity percentage estimate
+print "the cosine similarity percentage of string 1 and 2 is " + str(90 - degrees1) + " %"
+print "the cosine similarity percentage of string 1 and 3 is " + str(90 - degrees2) + " %"
--- cosineSim.py
+++ cosineSim.py
@@ -10,16 +10,19 @@ from sklearn.metrics.pairwise import cosine_similarity
 import math
 
 
+#transform documents into the TF-IDF matrix:
 tfidf_vectorizer = TfidfVectorizer()
 tfidf_matrix = tfidf_vectorizer.fit_transform(documents)
 print tfidf_matrix.shape
 (4, 11)
 
+#calculate the Cosine Similarity between the first document
+#with each of the other documents of the set:
 
-cosine_similarity(tfidf_matrix[0:1], tfidf_matrix)
-array = [ 1.        ,  0.36651513,  0.52305744,  0.13448867]
+cos_sim = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix)
+#array = [ 1.        ,  0.36651513,  0.52305744,  0.13448867]
 
-
+#check the angle between the first and third documents:
 # This was already calculated on the previous step, so we just use the value
 cos_sim = 0.52305744
 angle_in_radians = math.acos(cos_sim)
